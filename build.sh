7ee9511cf519c456e091b22a1650e709908c4e8c
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-03-26 00:04:11 +0100

    Rebuild with npm

--- build.sh
+++ build.sh
@@ -1,41 +1,33 @@
-#! /opt/homebrew/bin/bash
+#! /usr/bin/env bash
 
-fd '.*\.(css|html|js|json|md)' -E index.html --exec npx prettier --write {} &&
+set -Eeuo pipefail
 
-npx standard --fix switchTheme.js &&
-
-pandoc index-0.md \
--f markdown-auto_identifiers \
--M document-css=false \
---tab-stop=2 \
--H desc.html \
--H components/style-links-main.html \
--B components/header-main.html \
--A components/script-link-main.html \
--o index.html &&
-
-fd index.md \
---exec pandoc {} \
--f markdown-auto_identifiers \
--M document-css=false \
---tab-stop=2 \
--H '{//}'/desc.html \
--H components/style-links.html \
--B components/header.html \
--A components/footer.html \
--A components/script-link.html \
--o '{.}'.html &&
-
-fd index.html \
---exec npx html-minifier {} \
---collapse-whitespace \
---minify-css true \
---remove-comments \
--o {} &&
-
-fd index.html --exec sd '<style>.*</style>' '' {} &&
-
-fd index.html \
---exec sd 'a href="http' 'a target="_blank" rel="noopener" href="http' {} &&
-
-fd index.html --exec npx prettier --write {}
+sort-package-json && npm install &&
+	fd index.html -x rm {} &&
+	npm run prettify-all &&
+	npm run lint &&
+	pandoc index-0.md \
+		-f markdown-auto_identifiers \
+		-M document-css=false \
+		--tab-stop=2 \
+		-H desc.html \
+		-H components/style-links-main.html \
+		-B components/header-main.html \
+		-A components/script-link-main.html \
+		-o index.html &&
+	fd index.md \
+		-x pandoc {} \
+		-f markdown-auto_identifiers \
+		-M document-css=false \
+		--tab-stop=2 \
+		-H '{//}'/desc.html \
+		-H components/style-links.html \
+		-B components/header.html \
+		-A components/footer.html \
+		-A components/script-link.html \
+		-o '{.}'.html &&
+	npm run minify-html &&
+	fd index.html -x sd '<style>.*</style>' '' {} &&
+	fd index.html \
+		-x sd 'a href="http' 'a target="_blank" rel="noopener" href="http' {} &&
+	npm run prettify-html
